4ec2fa760f0234a3e579347ea3bc0bcaca408247
Frozenight <dabaseinas@gmail.com>
2023-04-19 13:18:51 +0300

    Fixed planet generation

---
+++ Assets/Scripts/AdvancedPlanets/CelestialBodySettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Celestial Body/Settings Holder")]
+public class Celestial_BodySettings : ScriptableObject
+{
+	public CelestialBodyShape shape;
+	public CelestialBodyShading shading;
+}
